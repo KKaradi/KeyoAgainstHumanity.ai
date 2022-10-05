f9c8e7b43244eccff896f8f36e8cab9a528cdd66
Kai Karadi <100025564@mvla.net>
2022-10-04 18:49:56 -0700

    fixed up

--- pages/api/predictions/[id].ts
+++ pages/api/predictions/[id].ts
@@ -8,11 +8,7 @@ export default async function handler(
     "https://api.replicate.com/v1/predictions/" + req.query.id,
     {
       headers: {
-<<<<<<< HEAD
-        Authorization: 'Token 64e7a82766f9504caeaf83a48b17419e635b4f78',
-=======
-        Authorization: "Token <PUT YOUR TOKEN HERE>",
->>>>>>> f1000a289e63e4559d9f94fab7e7d472adb9c9d1
+        Authorization: "Token <PUT IN API TOKEN>",
         "Content-Type": "application/json",
       },
     }
--- pages/api/predictions/index.ts
+++ pages/api/predictions/index.ts
@@ -1,30 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-<<<<<<< HEAD
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body.prompt)
-    const response = await fetch("https://api.replicate.com/v1/predictions", {
-      method: "POST",
-      headers: {
-        Authorization: 'Token 64e7a82766f9504caeaf83a48b17419e635b4f78',
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        version:
-=======
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log(req.body.prompt);
   const response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
     headers: {
-      Authorization: "Token <PUT YOUR TOKEN HERE>",
+      Authorization: "Token <PUT IN API TOKEN>",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
       version:
->>>>>>> f1000a289e63e4559d9f94fab7e7d472adb9c9d1
         "a9758cbfbd5f3c2094457d996681af52552901775aa2d6dd0b17fd15df959bef",
       input: { prompt: req.body.prompt },
     }),
