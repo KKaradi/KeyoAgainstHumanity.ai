34dfdeb534526810d8eb3c8679595116ce36c30f
MiracleNothing <themiraclenothing@gmail.com>
2022-10-04 19:55:14 -0500

    Sets up the latter half of firebase-util with functions. WIP

--- utils/firebase-utils/firebase-util.ts
+++ utils/firebase-utils/firebase-util.ts
@@ -1,3 +1,22 @@
+//Initializing Firebase
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, set, get, child} from "firebase/database"
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+// ...
+// The value of `databaseURL` depends on the location of the database
+databaseURL: "https://keyo-against-humanity-d9a9d-default-rtdb.firebaseio.com/",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase();
+const db = getDatabase();
+
+
+
 export async function createRoom(roomCode: number): Promise<void> {
   //write this method
   //add a room object, set started to false
@@ -27,24 +46,70 @@ export async function fetchImageURL(
   return "";
 }
 
+//I do past this remove at commit
+
 export async function uploadCaption(
   caption: string,
   yourUserName: string,
   roomCode: number
-): Promise<void> {}
+) : Promise<void> {
+  set(
+    ref(
+      database,
+      "Rooms/" +
+        roomCode +
+        "/" +
+        yourUserName +
+        "/" +
+        caption
+    ),
+    {
+      votes: 0,
+    }
+  );
+}
+
 
 export async function fetchListOfCaptions(
-  appleryourUserName: string,
+  applerUserName: string,
   roomCode: number
-): Promise<{ caption: string; authoryourUserName: string }[]> {
-  return [];
+): Promise<{ caption: string; authorUserName: string }[]> { 
+  const snapshot = await get(
+    child(
+      ref(database), 
+      "Rooms/" + 
+        roomCode + 
+        "/Userlist/") ); 
+      return [snapshot.val()];
+  /*get( 
+    ref(
+      database,
+      "Rooms/" +
+      roomCode +
+        "/" +
+        applerUserName
+    )
+  ) 
+  //This is the general idea, cannot get it working yet. Maybe remove return/list statments? -Z */
 }
 
 export async function vote(
-  appleryourUserName: string,
+  applerUserName: string,
   captionAuthor: string,
   roomCode: number
-): Promise<void> {}
+): Promise<void> {
+  get(
+    ref(
+      database,
+      "Rooms/" +
+        roomCode +
+        "/" +
+        applerUserName +
+        "/" +
+        captionAuthor
+    ),
+  );
+}
 
 export async function fetchVoteList(
   roomCode: number,
@@ -52,5 +117,7 @@ export async function fetchVoteList(
 ): Promise<
   { playeryourUserName: string; caption: string; numVotes: number }[]
 > {
-  return [];
+  return [
+    //get fetchListofCaptions working, this should fall right into place. -Z
+  ];
 }
