3a2220a2b597ecc6a557da5ecc37543d7d858a50
Kai Karadi <100025564@mvla.net>
2022-10-09 16:36:22 -0700

    testing

---
+++ pages/fireTest/test.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+// import { Routes, Route, useNavigate } from "react-router-dom";
+import Router from "next/router";
+import { SetStateAction, useState } from "react";
+import { createRoom } from "../../utils/firebase-utils/firebase-util";
+
+const Home: NextPage = () => {
+  const callBack1 = () => {
+    createRoom(100);
+  };
+  const callBack2 = () => {};
+  const callBack3 = () => {};
+  const callBack4 = () => {};
+  const callBack5 = () => {};
+  const callBack6 = () => {};
+
+  return (
+    <main>
+      <button onClick={() => callBack1}>Button 1</button>
+      <button onClick={() => callBack2}>Button 2</button>
+      <button onClick={() => callBack3}>Button 3</button>
+      <button onClick={() => callBack4}>Button 4</button>
+      <button onClick={() => callBack5}>Button 5</button>
+      <button onClick={() => callBack6}>Button 6</button>
+    </main>
+  );
+};
+
+export default Home;
