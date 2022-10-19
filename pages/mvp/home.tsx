45521f43f099cf6dc49ef2803ea5dca4eb8b8cde
Ipum8 <ailesh.sadruddin0408@gmail.com>
2022-10-19 08:11:16 -0500

    home.tsx

--- pages/mvp/home.tsx
+++ pages/mvp/home.tsx
@@ -1,66 +1,49 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 // import { Routes, Route, useNavigate } from "react-router-dom";
-import Router from 'next/router'
-import { SetStateAction, useState } from 'react'
-import {
-  getDatabase,
-  ref,
-  set,
-  get,
-  child,
-  update,
-  onChildAdded,
-} from 'firebase/database'
-const db = getDatabase()
+import Router from "next/router";
+import { SetStateAction, useState } from "react";
 
 const Home: NextPage = () => {
-  const [userName, setUserName] = useState('')
+
+  const [userName, setUserName] = useState("");
 
   const inputUserName = (event: {
-    target: { value: SetStateAction<string> }
+    target: { value: SetStateAction<string> };
   }) => {
-    setUserName(event.target.value)
-  }
+    setUserName(event.target.value);
+  };
 
-  let [roomID, setRoomID] = useState('')
+  let [roomID, setRoomID] = useState("");
 
   const inputRoomID = (event: {
-    target: { value: SetStateAction<string> }
+    target: { value: SetStateAction<string> };
   }) => {
-    setRoomID(event.target.value)
-  }
+    setRoomID(event.target.value);
+  };
 
   function createRoom() {
     //make this random
     let roomCode = 6720
     Router.push({
-      pathname: '/mvp/lobby',
+      pathname: "/mvp/lobby",
       query: {
         userName,
         roomCode,
       },
-    })
+    });
   }
 
   function navToLobby() {
     Router.push({
-      pathname: '/mvp/lobby',
+      pathname: "/mvp/lobby",
       query: {
         userName,
         roomID,
       },
-    })
-  }
-  const roomCode = 10000
-  async function test() {
-    const snapshot = await get(
-      child(ref(db), 'Rooms/' + roomCode + '/Userlist/'),
-    )
-
-    console.log(snapshot.val())
+    });
   }
 
   return (
@@ -87,14 +70,13 @@ const Home: NextPage = () => {
         />
       </div>
       <div>
-        <button onClick={() => createRoom()}>Create Room</button>
-        <button onClick={() => test()}>Create Room</button>
+        <button onClick={() => navToLobby()}>Join Room</button>
       </div>
       <div>
         <button onClick={() => createRoom()}>Create Room</button>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
